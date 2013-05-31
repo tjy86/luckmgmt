3f9f08e622ee1742530c56904a15f8b0d8dc5e77
tjy86 <thomasjyang@gmail.com>
2013-05-31 05:53:05 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Luckmgmt::Application.config.secret_token = '5aa1498592f2bb96fec3e9d154357c601797b379cac9e7f9f70b513b7fad93c96720d938bbdec14d7b2bd84b9a8880a86b49293e65474659a7a3c8da090335b5'
